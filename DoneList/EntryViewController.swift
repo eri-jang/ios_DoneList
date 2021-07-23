7079fdb52eb5d385606c3ec936089d8794658536
eri-jang <winterbs115@gmail.com>
2021-07-23 14:42:52 +0900

    done list done!

---
+++ DoneList/EntryViewController.swift
@@ -0,0 +1,56 @@
+//
+//  EntryViewController.swift
+//  ToDoList
+//
+//  Created by refine on 2021/07/22.
+//
+
+import RealmSwift
+import UIKit
+
+class EntryViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet var textField: UITextField!
+    @IBOutlet var datePicker: UIDatePicker!
+    
+    private let realm = try! Realm()
+    public var completionHandler: (() -> Void)?
+     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        textField.delegate = self
+        textField.becomeFirstResponder()
+        
+        
+        datePicker.setDate(Date(), animated: true)
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Save", style: .done, target: self, action: #selector(didTapSaveButton))
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        
+        return true
+    }
+    
+    @objc func didTapSaveButton() {
+        if let text = textField.text, !text.isEmpty {
+            let date = datePicker.date
+            
+            realm.beginWrite()
+            
+            let newItem = ToDoListItem()
+            newItem.date = date
+            newItem.item = text
+            realm.add(newItem)
+            try! realm.commitWrite()
+            
+            completionHandler?()
+            navigationController?.popToRootViewController(animated: true)
+        }
+        else {
+            print("Add something")
+        }
+    }
+
+}
--- DoneList/ViewController.swift
+++ DoneList/ViewController.swift
@@ -1,19 +1,90 @@
 //
 //  ViewController.swift
-//  DoneList
+//  ToDoList
 //
-//  Created by refine on 2021/07/23.
+//  Created by refine on 2021/07/21.
 //
 
+import RealmSwift
 import UIKit
 
-class ViewController: UIViewController {
+/*
+ - To show list of current to do list items
+ - To enter new to do list items
+ - To Show previously entered to do list item
+ 
+ - Item
+ - Date
+ 
+ */
+class ToDoListItem: Object {
+    @objc dynamic var item: String = ""
+    @objc dynamic var date: Date = Date()
+}
 
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet var table: UITableView!
+    
+    private let realm = try! Realm()
+    private var data = [ToDoListItem]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        data = realm.objects(ToDoListItem.self).map({ $0 })
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        table.delegate = self
+        table.dataSource = self
     }
-
-
+    
+    // Mark: Table
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text =  data[indexPath.row].item
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        //Open the screen where we can see item info and delete
+        let item = data[indexPath.row]
+        
+        guard let vc = storyboard?.instantiateViewController(identifier: "view") as? ViewViewController else {
+            return
+        }
+        
+        vc.item = item
+        vc.deletionHandler = { [weak self] in
+            self?.refresh()
+            
+        }
+        vc.navigationItem.largeTitleDisplayMode = .never
+        vc.title = item.item
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    @IBAction func didTapAddButton() {
+        guard let vc = storyboard?.instantiateViewController(identifier: "enter") as? EntryViewController else {
+            return
+        }
+        vc.completionHandler = { [weak self] in
+            self?.refresh()
+        }
+        vc.title = "New Item"
+        vc.navigationItem.largeTitleDisplayMode = .never
+        navigationController?.pushViewController(vc, animated: true)
+        
+    }
+    
+    func refresh() {
+        data = realm.objects(ToDoListItem.self).map({ $0 })
+        table.reloadData()
+    }
 }
 
