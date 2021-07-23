b853077eb71550d7e6f251f23765d3acbfafe928
eri-jang <winterbs115@gmail.com>
2021-07-23 15:33:30 +0900

    add delete alert

--- DoneList/ViewViewController.swift
+++ DoneList/ViewViewController.swift
@@ -31,13 +31,37 @@ class ViewViewController: UIViewController {
         itemLabel.text = item?.item
         dateLabel.text = Self.dateFormatter.string(from: item!.date)
         
-        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .trash, target: self, action: #selector(didTapDelete))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .trash, target: self, action: #selector(touchDeleteButton))
+        
+    }
+    
+    //alert
+    func showAlert(message: String){
+        
+        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        
+        let cancelAction = UIAlertAction(title: "cancel", style: .default, handler: nil)
+
+        let okAction = UIAlertAction(title: "DELETE", style: .destructive) { action in
+            self.didTapDelete()
+        }
+        
+        //let okAction = UIAlertAction(title: "Delete", style: .default){(action) in self.didTapDelete()}
+        alert.addAction(cancelAction)
+        alert.addAction(okAction)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    @objc private func touchDeleteButton() {
+        showAlert(message: "삭제하시겠습니까?")
     }
     
     @objc private func didTapDelete() {
+        
         guard let myItem = self.item else {
             return
         }
+        
         realm.beginWrite()
         realm.delete(myItem)
         try! realm.commitWrite()
